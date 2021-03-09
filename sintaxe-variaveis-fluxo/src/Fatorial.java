c1cd300377964f022d77a115bdf1947423f15b87
Raimundo Rodrigues Lima <raimundo.lima@b2wdigital.com>
2021-03-09 09:54:30 -0300

    first commit

---
+++ sintaxe-variaveis-fluxo/src/Fatorial.java
@@ -0,0 +1,12 @@
+
+public class Fatorial {
+	public static void main(String[] args) {
+		int f = 1;
+		for(int i = 1; i <= 10; i++) {
+			f *= i;
+			System.out.println("Fatorial de " + i + " = " + f);
+		}
+		
+	}
+
+}
